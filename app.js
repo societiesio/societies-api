2c6332fb960fc4245c9280f327e5c29f67bb2e5b
Paulo Diovani <paulo@diovani.com>
2017-03-27 00:45:13 -0300

    Add basic error handling

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require('dotenv').config({ silent: true })
+const error = require('debug')(`${process.env.npm_package_name}:web:error`)
 
 const Koa = require('koa')
 const app = new Koa()
@@ -7,5 +8,7 @@ app.use(require('./middlewares/logger'))
 app.use(require('./middlewares/response_time'))
 
 app.use(require('./routes/index'))
+
+app.on('error', (err) => error('server error', err))
 
 module.exports = app
