c6aeeb274a3068ba7fa9babcaa7ffb056fd5aee1
Paulo Diovani <paulo@diovani.com>
2017-04-06 15:03:17 -0300

    Use koa-generator for koa2

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,17 @@
-module.exports = (ctx) => {
-  ctx.body = 'Hello world'
-}
+const router = require('koa-router')()
+
+router.get('/', async function (ctx, next) {
+  ctx.state = {
+    title: 'koa2 title'
+  }
+
+  await ctx.render('index', {})
+})
+
+router.get('/foo', async function (ctx, next) {
+  await ctx.render('index', {
+    title: 'koa2 foo'
+  })
+})
+
+module.exports = router
